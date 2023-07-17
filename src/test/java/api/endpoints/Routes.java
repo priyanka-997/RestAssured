070ada197ccf5aaf179baa4db2dcda5f9828a459
priyanka-997 <priyanka997sharma@gmail.com>
2023-07-17 16:12:09 +0530

    class modified

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -1,7 +1,6 @@
 package api.endpoints;
 
-public class Routes {
-	
+public class Routes {	
 	
 public static String base_url=  "https://petstore.swagger.io/v2";
 
@@ -10,5 +9,4 @@ public static String get_url = base_url+"/user/{username}";
 public static String update_url = base_url+"/user/{username}";
 public static String delete_url = base_url+"/user/{username}";
 	
-
 }
